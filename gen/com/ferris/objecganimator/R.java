05ba8cb176d156d19223b129f5452a3bf19a83e0
xufeifandj <459821731@qq.com>
2014-09-29 21:22:37 +0800

    view的影响动画
    view的出现对其他view造成的影响

--- gen/com/ferris/objecganimator/R.java
+++ gen/com/ferris/objecganimator/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1109,6 +1109,11 @@ containing a value of this type.
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
+        public static final int id_appear=0x7f05003f;
+        public static final int id_change_appear=0x7f050040;
+        public static final int id_change_disappear=0x7f050042;
+        public static final int id_container=0x7f05003e;
+        public static final int id_disappear=0x7f050041;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
@@ -1195,10 +1200,12 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_main_activity2=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int main_activity2=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1244,6 +1251,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_main_activity2=0x7f0a0010;
     }
     public static final class style {
         /** 
